8637badafe6fb5745cc8b531b6eaf2880c6a880f
Владислав Павлик <144818450+TerryCollians@users.noreply.github.com>
2023-12-09 19:17:16 +0100

    fix

--- telegram.php
+++ telegram.php
@@ -3,25 +3,26 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 
-$name htmlspecialchars($name);
-$email htmlspecialchars($email);
+$name = htmlspecialchars($name);
+$email = htmlspecialchars($email);
 
-$name urldecode($name);
-$email urldecode($email);
+$name = urldecode($name);
+$email = urldecode($email);
 
-$name trim($name);
-$email trim($email);
+$name = trim($name);
+$email = trim($email);
 
-if  (mail("terrycollians@terry.zzz.com.ua",
+if  (mail("johnylikonsky@gmail.com",
           "New message on site Castaway: ",
           "Name: ".$name."\n".
-          "Email: ".$email."\n".)
+          "Email: ".$email."\n",
+          "From: terrycollians@terry.zzz.com.ua \r\n")
 ) {
-  echo ('Message sended!')
+  echo ('Message sended!');
 }
 
 else{
-  echo('Error... Check data')
+  echo('Error... Check data');
 }
 
 ?>
